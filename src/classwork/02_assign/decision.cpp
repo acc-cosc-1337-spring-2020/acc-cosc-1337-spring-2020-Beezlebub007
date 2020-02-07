db895f2caa9289cf32b0da42b9d1cba68a75bd1c
Beezlebub007 <Dylan.bell2@g.austincc.edu>
2020-02-06 19:55:01 -0600

    Class Assignment 2

--- src/classwork/02_assign/decision.cpp
+++ src/classwork/02_assign/decision.cpp
@@ -4,7 +4,9 @@ double gross_pay(double hours, double hourly_rate)
 {
 	if (hours > 40)
 	{
-		return ((hours * hourly_rate)+ (hours - 40)*(hourly_rate*1.5));
+		double pay;
+		pay = 40 * hourly_rate;
+		return (pay + (hours - 40)*(hourly_rate*1.5));
 	}
 	else if (hours <= 40)
 	{
