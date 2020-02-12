bb4f3e5d8e6eb352eec4cbb3f497ed493ebb75ab
Beezlebub007 <Dylan.bell2@g.austincc.edu>
2020-02-11 18:44:04 -0600

    example 3 for loop

--- src/examples/03_module/02_for/for.cpp
+++ src/examples/03_module/02_for/for.cpp
@@ -4,6 +4,14 @@
 //Write code for function display_numbers that accepts a number and 
 //displays every number that precedes(starting from 1) it in a line
 //Example: display_numbers(5)
+void display_numbers(int num)
+{
+	for (int i = 0; i < num; ++i)
+	{
+		std::cout << i +1 << "\n";
+	}
+}
+
 /*
 RESULT:
 
