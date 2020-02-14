96f42b917b9293bc818ae8a206145f9042a85977
Beezlebub007 <Dylan.bell2@g.austincc.edu>
2020-02-13 18:46:31 -0600

    for range and value reference

--- src/examples/03_module/04_for_ranged/main.cpp
+++ src/examples/03_module/04_for_ranged/main.cpp
@@ -4,6 +4,12 @@
 
 int main() 
 {
+	auto name = "john";// auto can determine type of data as long as a value is given.
+	auto num = 5;
+	auto num1 = 5.5;
+	std::string str = "joe";
 
+	loop_string_w_index(str);
+	loop_string_w_auto(str);
 	return 0;
 }
--- test/examples_test/03_module_test/03_module_tests.cpp
+++ test/examples_test/03_module_test/03_module_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "while.h"
+#include "value_ref.h"
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
@@ -12,3 +13,12 @@ TEST_CASE("Test Sum of squares function")
 	REQUIRE(sum_of_squares(6) == 91);
 }
 
+TEST_CASE("Test pass val and ref")
+{
+	int num1 = 5, num2 = 10;
+	pass_by_val_and_ref(num1, num2);
+
+	REQUIRE(num1 == 5);
+	REQUIRE(num2 == 50);
+
+}
