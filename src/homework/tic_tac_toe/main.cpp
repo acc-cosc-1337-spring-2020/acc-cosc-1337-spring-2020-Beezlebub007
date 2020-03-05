cb6e2208c297682af3dc1c52ef061b9780683293
Beezlebub007 <dylan.bellxd@gmail.com>
2020-03-05 15:39:27 -0600

    Edit TicTacToe

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -3,24 +3,25 @@
 using std::cout; using std::cin;
 int main() 
 {
+	tictactoe start;
 	char choice;
 	string letter;
 	int mark;
-
+	auto player = start.get_player();
+	cout << "Player One select X or O " << "\n";
+	cin >> player;
 	
+	try
+	{
+		start.start_game(player);
+	}
+	catch (Error e)
+	{
+		cout << e.get_error() << "\n";
+	}
 	do
 	{ 
-		cout << "Player One select X or O " << "\n";
-		cin >> letter;
-		tictactoe start;
-		try
-		{
-			start.start_game(letter);
-		}
-		catch (Error e)
-		{
-			cout << e.get_error() << "\n";
-		}
+		
 		cout << "Select position 1-9 " << "\n";
 		cin >> mark;
 		try
@@ -31,6 +32,7 @@ int main()
 		{
 			cout << f.get_error() << "\n";
 		}
+		cout << mark << " = " << player << "\n";
 		cout << "continue? Y or N.";
 		cin >> choice;
 	} while (choice == 'Y'|| choice == 'y');
