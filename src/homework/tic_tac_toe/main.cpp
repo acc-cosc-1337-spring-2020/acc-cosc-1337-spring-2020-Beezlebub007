5c070b604dd71a78c706579da06bab05f0ed83b3
Beezlebub007 <dylan.bellxd@gmail.com>
2020-04-02 20:25:09 -0500

    Homework 7 main

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -7,34 +7,48 @@ int main()
 	char choice;
 	string letter;
 	int mark;
+	int play = 1;
+	bool win = false;
 	auto player = start.get_player();
-	cout << "Player One select X or O " << "\n";
-	cin >> player;
-	
-	try
-	{
-		start.start_game(player);
-	}
-	catch (Error e)
-	{
-		cout << e.get_error() << "\n";
-	}
 	do
-	{ 
-		
-		cout << "Select position 1-9 " << "\n";
-		cin >> mark;
-		try
+	{
+		do {
+			play = 1;
+			cout << "Player One select X or O " << "\n";
+			cin >> player;
+			try
+			{
+				start.start_game(player);
+			}
+			catch (Error e)
+			{
+				cout << e.get_error() << "\n";
+				play = 0;
+			}
+		} while (play != 1);
+		start.display_board();
+		do
 		{
-			start.mark_board(mark);
-		}
-		catch (Error f)
-		{
-			cout << f.get_error() << "\n";
-		}
-		cout << mark << " = " << player << "\n";
-		cout << "continue? Y or N.";
-		cin >> choice;
+			
+			cout << "Select position 1-9 " << "\n";
+			cin >> mark;
+			try
+			{
+				start.mark_board(mark);
+			}
+			catch (Error f)
+			{
+				cout << f.get_error() << "\n";
+			}
+			cout << mark << " = " << player << "\n";
+			start.display_board();
+			start.game_over();
+			
+			win = start.game_over();
+		} while (win == false);
+	cout << "winner is " << start.get_winner() << "\n";
+	cout << "continue? Y or N.";
+	cin >> choice;
 	} while (choice == 'Y'|| choice == 'y');
 	{
 
