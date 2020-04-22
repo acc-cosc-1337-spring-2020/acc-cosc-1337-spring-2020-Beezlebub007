c909fcd91ee8b16b097f958719d60cf42470fddc
Beezlebub007 <dylan.bellxd@gmail.com>
2020-04-21 19:11:37 -0500

    Class copy with static a nd dynamic variables with copy constructor

--- src/classwork/11_assign/vector.h
+++ src/classwork/11_assign/vector.h
@@ -1,2 +1,25 @@
 //
 //TODO 7 add template
+
+
+#ifndef MY_VECTOR_H
+#define MY_VECTOR_H
+#include<cstddef>			//allows use for Mac OS
+#include<stddef.h>
+
+class Vector
+{
+public:
+	Vector(size_t sz);
+	Vector(const Vector& v);  // copy constructor
+	size_t Size()const { return size; }
+	//void set_size(size_t sz) { size = sz; }
+	int& operator[](int i) { return nums[i]; }
+	int& operator[](int i)const { return nums[i]; }
+private:
+	size_t size;
+	int* nums; // raw array
+};
+
+
+#endif //!1
